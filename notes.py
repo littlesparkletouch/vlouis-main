3732b5fd9d1fda7f93b064d5c11699259c224bf3
littlesparkletouch <victorialouis777@gmail.com>
2025-08-22 15:14:44 +0000

    Main

---
+++ notes.py
@@ -0,0 +1,23 @@
+# def is the keyword to define a funtion
+# whatType is the name of the funciton
+# userInput is the parameter of the funciton
+def whatType(userInput)
+    # print is a Python built-in funciton that prints tp the soncole
+    #type is a python built-in function that finds the datatype
+    #userInput is the variable that the user enters
+    print(type(userInput))
+
+# The pound symbol is for one line comments
+# The program ignores all comments
+#Call the function with different user inputs
+"""
+
+Three quotes is for multiple line comments
+
+"""
+#call the funtion with different user unoputs
+whatType(3)
+whatType(3.0)
+whatType(True)
+whatType("polyana")
+whatType('p')
